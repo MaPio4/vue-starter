8386cde6e50c8ec75d028bca1719e99ffe4c8479
vKapseLv <mp1245@wp.pl>
2023-04-16 15:30:25 +0200

    Created new component for Login purposes.

--- src/App.vue
+++ src/App.vue
@@ -3,60 +3,39 @@
 
     <h1>Witaj w systemie do zapisu na zajęcia!</h1>
 
-    <div v-bind:hidden="loggedIn">Zaloguj się mailem:</div>
-    <input v-bind:hidden="loggedIn" type="email" v-model="email" :on-change="validateMail()">
-    <button v-bind:hidden="loggedIn" @click="login()"> Zaloguj</button>
-    <div v-bind:hidden="loggedIn">{{ errorMsg }}</div>
+    <div v-if="email.length <= 0">
+      <LoginForm @login="(username) => logMeIn(username)"></LoginForm>
+    </div>
 
-    <h2 v-bind:hidden="!loggedIn">Witaj {{ email }}</h2>
-    <div v-bind:hidden="!loggedIn" @click="logout()">Wyloguj</div>
+    <div v-else>
+      Zalogowano jako {{ email }} <br>
+      <button @click="logMeOut()">Wyloguj</button>
+    </div>
+
   </div>
 </template>
 
 <script>
 import "milligram";
+import LoginForm from "./LoginForm";
 export default {
   data() {
     return {
       email: '',
-      password: '',
-      loggedIn: false,
-      errorMsg: '',
     };
   },
   methods: {
-  alertMyEmail() {
-    alert(this.email);
+    logMeIn(username) {
+      this.email = username;
+      console.log(`Logged in as ${username}`);
+    },
+    logMeOut() {
+      this.email = "";
+    },
   },
-  validateMail() {
-    if(this.email.length == 0) {
-      this.errorMsg = ``;
-      return false;
-    }
-    if(this.email.length < 6) {
-      this.errorMsg = `Email o długości ${this.email.length} to zbyt krótki mail.`;
-      return false;
-    }
-    else if(this.email.length > 30) {
-      this.errorMsg = `Email o długości ${this.email.length} to zbyt długi mail.`;
-      return false;
-    }
-    else if(!this.email.includes('@')) {
-      this.errorMsg = `Email nie posiada znaku @`;
-      return false;
-    }
-    else {
-      this.errorMsg = "";
-      return true;
-    }
+  components: {
+    LoginForm,
   },
-  login() {
-    this.loggedIn = this.validateMail();
-  },
-  logout() {
-    this.loggedIn = false;
-  },
-}
 }
 </script>
 
---
+++ src/LoginForm.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+      <label>Zaloguj się e-mailem</label>
+      <input type="email" v-model="email">
+      <button @click="enter()">Zaloguj się</button>
+      <div>{{ errorMsg }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+data() {
+    return {
+        email: '',
+        errorMsg: '',
+    }
+},
+methods: {
+    enter() {
+      if(this.validateMail(this.email))
+        this.$emit('login', this.email);
+    },
+    validateMail(mail) {
+      if(mail.length < 6) {
+        this.errorMsg = `Email o długości ${mail.length} to zbyt krótki mail.`;
+        return false;
+      }
+      else if(mail.length > 30) {
+        this.errorMsg = `Email o długości ${mail.length} to zbyt długi mail.`;
+        return false;
+      }
+      else if(!mail.includes('@')) {
+        this.errorMsg = `Email nie posiada znaku @`;
+        return false;
+      }
+      else {
+        this.errorMsg = "";
+        return true;
+      }
+    },
+  }
+}
+</script>
